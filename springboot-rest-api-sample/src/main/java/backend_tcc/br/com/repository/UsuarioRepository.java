736356d3844ecfd819aca90361986e3f9a706cff
LucasF4 <superlucas2005@gmail.com>
2022-09-05 14:02:53 -0300

    commit para salvar o crud de usuario

--- springboot-rest-api-sample/src/main/java/backend_tcc/br/com/repository/UsuarioRepository.java
+++ springboot-rest-api-sample/src/main/java/backend_tcc/br/com/repository/UsuarioRepository.java
@@ -13,6 +13,6 @@ import backend_tcc.br.com.model.Usuario;
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 	
 	@Query (value = "select u from Usuario u where upper(trim(u.name)) like %?1%")
-	List<Usuario> getEveryUser(String name );
+	List<Usuario> getEveryUser(String name);
 
 }
